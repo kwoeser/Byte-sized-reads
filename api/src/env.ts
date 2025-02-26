2de3b9c5b13b52688fc603cfedf134020133aba2
hazel <hazel@meows.zip>
2025-02-26 14:10:38 -0800

    Fix reading PORT env variable

--- api/src/env.ts
+++ api/src/env.ts
@@ -2,7 +2,10 @@ import { z } from "zod";
 
 const envSchema = z.object({
   DATABASE_URL: z.string(),
-  PORT: z.number().nullish(),
+  PORT: z.preprocess((input) => {
+    if (typeof input === "string") return parseInt(input);
+    return input;
+  }, z.number().nullish()),
 });
 
 export const env = envSchema.parse(process.env);
