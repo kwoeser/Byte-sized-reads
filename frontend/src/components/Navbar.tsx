49eb1290897847da05c3931b1c0d51b0316db836
anmaiiii <anmai8376@gmail.com>
2025-03-05 12:39:53 -0800

    Update Navbar.tsx
    added icons, fixed spacing

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import Login from "./Login";
 import SearchBar from "./SearchBar";
 import DropdownFilter from "./DropdownFilter";
+import { Home, Book } from "lucide-react";
 
 
 const Navbar = () => {
@@ -9,16 +10,26 @@ const Navbar = () => {
 
     <nav className="fixed w-full bg-white text-black shadow-md border-b border-gray-100">
       
-      <div className="flex items-center px-8 py-3">
+      <div className="flex items-center justify px-8 py-3">
 
+        {/* Left side */}
         {/* navbar links */}
-        <div className="space-x-6">
-          <Link to="/" className="hover:text-red-500">Home</Link>
-          <Link to="/Library" className="hover:text-red-500">Our Library</Link>
+        <div className="flex items-center space-x-6">
+          <Link to="/" className="flex items-center hover:text-red-500">
+           <Home className="mr-2" size={18} />
+           <span>Home</span>
+        </Link>
+        <Link to="/Library" className="flex items-center hover:text-red-500">
+          <Book className="mr-2" size={18} />
+          <span>Our Library</span>
+         </Link>
         </div>
-        <DropdownFilter/>
+        <DropdownFilter />
+        </div>
        
+        {/* Rghit side */}
         {/* might move search bar off navbar onto middle of page or on the top right of the home page */}
+        <div className="flex items-center space-x-4">
         <SearchBar/>
         
         <Link to="/LoginPage"><Login/></Link>
