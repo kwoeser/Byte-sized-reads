6b05dff17b4fdd051fe8b96a86827582629c4271
kwoeser <karmawoeser1@gmail.com>
2025-03-23 22:35:00 -0700

    aws

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,10 +7,14 @@ export default defineConfig({
 
   // https://vite.dev/config/server-options#server-proxy
   server: {
+
     port: 5173,
     proxy: {
       "/api": {
-        target: "http://localhost:5174",  
+        // target: "http://localhost:5174",  
+
+        // AWS PORT
+        target: "http://3.147.55.38:5174",
         changeOrigin: true,
         // removes /api
         rewrite: (path) => path.replace(/^\/api/, ""), 
